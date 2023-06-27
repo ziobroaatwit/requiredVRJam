35072cd4faedbcf8029f82529ebed4d5425213b5
wanga4 <59738263+wanga4@users.noreply.github.com>
2023-06-27 00:26:33 -0400

    Added Nav Mesh
    Added a Nav Mesh and added some AI to walk around the world

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent (typeof(NavMeshAgent))]
+public class AIController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    [Range(0, 100)] public float speed;
+    [Range(1, 500)] public float walkRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent != null)
+        {
+            agent.speed = speed;
+            agent.SetDestination(RandomNavMeshLocation());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent != null && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.SetDestination(RandomNavMeshLocation());
+        }
+    }
+
+    public Vector3 RandomNavMeshLocation()
+    {
+        Vector3 finalPos = Vector3.zero;
+        Vector3 randomPos = UnityEngine.Random.insideUnitSphere * walkRadius;
+        randomPos += transform.position;
+        if(NavMesh.SamplePosition(randomPos, out NavMeshHit hit, walkRadius, 1))
+        {
+            finalPos = hit.position;
+        }
+        return finalPos;
+    }
+}
