397c598bc8d86c09fcd57a519ddfac5f2d53e5e8
colesdatwit <colesd@wit.edu>
2023-06-30 14:02:37 -0400

    Added TextMeshPro for floating scoreboard

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreController : MonoBehaviour
 {
     public int score = 0;
+    public TextMeshPro mText;
 
     void OnGUI()
     {
@@ -12,6 +14,7 @@ public class ScoreController : MonoBehaviour
         scoreStyle.fontSize = 24;
         scoreStyle.normal.textColor = Color.white;
         GUI.Label(new Rect(10, 10, 200, 50), "Score: " + score, scoreStyle);
+        mText.text = ("Score: "+score);
     }
 
 }
